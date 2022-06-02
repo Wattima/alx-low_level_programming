11e9d6642552b61e50b60a4a26bfd0a75de32f12
Daryl Wattima <darwattima@gmail.com>
2022-06-02 14:38:12 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long int lintType;
+	long long int llintType;
+	float floatType;
+
+	printf("Size of a char: %zu byte\n", sizeof(charType));
+	printf("Size of an int: %zu bytes\n", sizeof(intType));
+	printf("Size of a long int: %zu bytes\n", sizeof(lintType));
+	printf("Size of a long long int: %zu bytes\n", sizeof(llintType));
+	printf("Size of a float: %zu bytes\n", sizeof(floatType));
+	return (0);
+}
