61b3775b97f22e34441d711fd4e828005b9f87d0
Daryl Wattima <darwattima@gmail.com>
2022-06-07 11:59:04 -0700

    push

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ * Description - checks for lowercase character
+ * @c: character to check
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
+
