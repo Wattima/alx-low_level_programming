fc985b16bf091fc2ea253bdf6d9618244cb2680e
Daryl Wattima <darwattima@gmail.com>
2022-06-07 10:50:12 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * main - Entry point
+ * prints _putchar followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char c;
+	char d[] = "_putchar\n";
+
+	while (i <= 8)
+	{
+		c = d[i];
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
