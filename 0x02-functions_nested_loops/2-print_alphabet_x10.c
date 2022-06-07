e7cc7247d916bc427cc9564bfa06bca136507785
Daryl Wattima <darwattima@gmail.com>
2022-06-07 11:48:13 -0700

    push

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * print_alphabet_x10 - Entry point
- * Description - prints 10 times the alphabet, in lowercase, followed by a new line
+ * Description - prints 10 times the alphabet,
+ * in lowercase, followed by a new line
  * Return: Always o
  */
 void print_alphabet_x10(void)
