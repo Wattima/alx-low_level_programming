9e2e81fb2769a8c278668a2b0a50c575d6320e1c
Daryl Wattima <darwattima@gmail.com>
2022-06-07 11:34:52 -0700

    push

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - Entry point
+ *
+ * Description - prints the alphabet, in lowercase, followed by a new line
  *
  * Return: Always 0.
  */
