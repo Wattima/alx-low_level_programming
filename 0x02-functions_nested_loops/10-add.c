2c2b676f2aed5219af8645b784fccfd31c1f6186
Daryl Wattima <darwattima@gmail.com>
2022-06-07 14:45:17 -0700

    addd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * Desc - adds two integers and returns the result
+ * @i: first integer
+ * @j: second integer
+ * Return: Sum
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
