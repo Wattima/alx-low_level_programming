bb154f349c21e2f541dbcba29881423f96e99dbc
Daryl Wattima <darwattima@gmail.com>
2022-06-07 12:34:05 -0700

    push

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * Desc - computes the absolute value of an integer
+ * @int: integer checked
+ * Return: int
+ */
+int _abs(int)
+{
+	int (r >= 0);
+		return (r);
+
+	else
+	{
+		r *= -1;
+		return (r);
+	}
+}
