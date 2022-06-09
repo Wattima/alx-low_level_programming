5c336ba0ab1bb6c991c4f06908b1fcedf1539da0
Daryl Wattima <darwattima@gmail.com>
2022-06-09 01:10:47 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - Entry point
+ * Desc - checks for a digit (0 through 9)
+ * @c: character checked
+ * Return: 1 0r 0
+ */
+int _isdigit(int c)
+{
+	if ((c >= 0) && (c <= 9))
+		return (1);
+	else
+		return (0);
+}
