ece53213915bd83057b9d42eb2892e24a40f3234
Daryl Wattima <darwattima@gmail.com>
2022-06-09 14:06:45 -0700

    push

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry
+ * @n: the number of times char prints
+ * Return: diagonal line
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n > 0)
+	{
+		for (a = 0 ; a < n ; a++)
+		{
+			for (b = 0 ; b < a ; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
