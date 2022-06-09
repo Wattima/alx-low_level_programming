a58bf3b8f1f5bbe1234799e3463586cc945a8fee
Daryl Wattima <darwattima@gmail.com>
2022-06-09 01:19:58 -0700

    product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * Desc - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
