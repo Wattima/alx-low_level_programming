c308a7399a346844976ba4fe4074e4ac7d67224e
Daryl Wattima <darwattima@gmail.com>
2022-06-16 05:21:23 -0700

    2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @dest: first param
+ * @src: second param
+ * @n: bytes
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
