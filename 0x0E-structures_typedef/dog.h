df8c2d7c5ac23e210cee016f8a6efe19af17cb45
Daryl Wattima <darwattima@gmail.com>
2022-07-04 06:34:45 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - my struct
+ * @name: first member
+ * @age: second
+ * @owner: third
+ *
+ * Description: Dog details
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
